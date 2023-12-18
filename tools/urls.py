fc1b0f39d1a529adf62991bb272e70507f4c3ef7
comfortchambeshi <comfortchambeshi4@gmail.com>
2023-12-18 12:52:58 +0200

    new file:   chatbot/__init__.py 	new file:   chatbot/__pycache__/__init__.cpython-310.pyc 	new file:   chatbot/__pycache__/settings.cpython-310.pyc 	new file:   chatbot/__pycache__/urls.cpython-310.pyc 	new file:   chatbot/__pycache__/wsgi.cpython-310.pyc 	new file:   chatbot/asgi.py 	new file:   chatbot/settings.py 	new file:   chatbot/urls.py 	new file:   chatbot/wsgi.py 	new file:   db.sqlite3 	new file:   manage.py 	new file:   tools/__init__.py 	new file:   tools/__pycache__/__init__.cpython-310.pyc 	new file:   tools/__pycache__/admin.cpython-310.pyc 	new file:   tools/__pycache__/apps.cpython-310.pyc 	new file:   tools/__pycache__/models.cpython-310.pyc 	new file:   tools/__pycache__/urls.cpython-310.pyc 	new file:   tools/__pycache__/views.cpython-310.pyc 	new file:   tools/admin.py 	new file:   tools/apps.py 	new file:   tools/migrations/__init__.py 	new file:   tools/migrations/__pycache__/__init__.cpython-310.pyc 	new file:   tools/models.py 	new file:   tools/templates/tools/chatgpt.html 	new file:   tools/templates/tools/fetch_image.html 	new file:   tools/tests.py 	new file:   tools/urls.py 	new file:   tools/views.py

---
+++ tools/urls.py
@@ -0,0 +1,17 @@
+# chat_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import *
+
+app_name = "tools"
+
+urlpatterns = [
+    path('', chat, name='tools_home'),  # Render the chat interface
+    path('chatbot/', chat, name='chatbot'),  # Render the chat interface
+    path('generate_response/', generate_response, name='generate_response'),  # Handle AJAX requests
+    path('fetch_images/', fetch_images, name='fetch_image'),  # Handle AJAX requests
+
+
+]
